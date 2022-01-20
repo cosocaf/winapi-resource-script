0a20d07d8bf55f00a61b7a125590bb6955543720
cosocaf <cosocaf@gmail.com>
2022-01-21 01:24:18 +0900

    Allowed input from files for debugging purposes.

--- language-server/src/main.cpp
+++ language-server/src/main.cpp
@@ -2,11 +2,12 @@
 #include <io.h>
 
 #include <cstdio>
+#include <fstream>
 #include <iostream>
 
 #include "server.h"
 
-int main() {
+int main(int argc, char **argv) {
   using namespace winrsls;
 
   std::ios_base::sync_with_stdio(false);
@@ -14,7 +15,18 @@ int main() {
   _setmode(_fileno(stdout), _O_BINARY);
   std::ios_base::sync_with_stdio(true);
 
-  Server server(std::cin, std::cout);
-  const auto result = server.start();
-  return result;
+  if (argc == 3 && argv[1] == std::string("--input")) {
+    std::ifstream input(argv[2], std::ios_base::binary);
+    if (!input) {
+      std::cout << "Can't open input file: " << argv[2] << std::endl;
+      return -1;
+    }
+    Server server(input, std::cout);
+    const auto result = server.start();
+    return result;
+  } else {
+    Server server(std::cin, std::cout);
+    const auto result = server.start();
+    return result;
+  }
 }
